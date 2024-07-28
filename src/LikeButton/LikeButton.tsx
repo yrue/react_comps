0133f05b80692b691cc101ca2b51fcd4eb2ac333
eury.tang <eury0811@gmail.com>
2024-07-27 21:15:53 -0700

    chore: fix styles

--- src/LikeButton/LikeButton.tsx
+++ src/LikeButton/LikeButton.tsx
@@ -1,18 +1,18 @@
-import {} from 'react';
+import { } from 'react';
 import './LikeButton.module.css';
 import { HeartIcon, SpinnerIcon } from './icons';
 
 const LikeButton = () => {
     return (
         <div>
-          <button>
-            <HeartIcon /> Like
-          </button>
-          <button>
-            <SpinnerIcon /> Like
-          </button>
+            <button>
+                <HeartIcon /> Like
+            </button>
+            <button>
+                <SpinnerIcon /> Like
+            </button>
         </div>
-      );
+    );
 };
 
 export default LikeButton;

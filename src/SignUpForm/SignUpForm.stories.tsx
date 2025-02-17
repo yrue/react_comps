cf93a0aa91505cb86362cfb3ceb262f9d5bafd77
eury.tang <eury0811@gmail.com>
2025-02-17 21:46:11 +0900

    test(SignUpForm): check el case sensitively

--- src/SignUpForm/SignUpForm.stories.tsx
+++ src/SignUpForm/SignUpForm.stories.tsx
@@ -14,24 +14,24 @@ export const Default = {
         expect(canvasElement).toHaveTextContent('Confirm Password');
 
         // 2. Show invalid error when input invalid value for each fields
-        const usernameInput = within(canvasElement).getByRole('textbox', { name: /username/i });
+        const usernameInput = within(canvasElement).getByRole('textbox', { name: /Username/i });
         await userEvent.type(usernameInput, 'a');
         expect(canvasElement).toHaveTextContent('Minimal of 4 characters.');
 
-        const emailInput = within(canvasElement).getByRole('textbox', { name: /email/i });
+        const emailInput = within(canvasElement).getByRole('textbox', { name: /Email/ });
         await userEvent.type(emailInput, 'invalidemail');
         expect(canvasElement).toHaveTextContent('Invalid email address.');
 
-        const passwordInput = within(canvasElement).getByRole('textbox', { name: /password/i });
+        const passwordInput = within(canvasElement).getByRole('textbox', { name: /^Password/ });
         await userEvent.type(passwordInput, 'short');
         expect(canvasElement).toHaveTextContent('Minimal of 6 characters.');
 
-        const passwordConfirmInput = within(canvasElement).getByRole('textbox', { name: /confirm password/i });
+        const passwordConfirmInput = within(canvasElement).getByRole('textbox', { name: /Confirm Password/ });
         await userEvent.type(passwordConfirmInput, 'mismatch');
         expect(canvasElement).toHaveTextContent('Passwords do not match.');
 
         // 3. click submit to call API
-        const submitButton = within(canvasElement).getByRole('button', { name: /submit/i });
+        const submitButton = within(canvasElement).getByRole('button', { name: /Sign Up/ });
         await userEvent.click(submitButton);
 
         // 4. handle API response

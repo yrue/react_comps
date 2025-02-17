134d38f36b294ab53354be917e09272abf59959f
eury.tang <eury0811@gmail.com>
2025-02-17 16:05:02 +0900

    feat(SignUpForm): improved

--- src/SignUpForm/SignUpForm.tsx
+++ src/SignUpForm/SignUpForm.tsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
-
-enum UserInfoFields{
+import styles from './SignUpForm.module.scss'
+enum UserInfoFields {
     username = 'username',
     email = 'email',
     password = 'password',
-    confirm= 'passwordConfirm'
+    confirm = 'passwordConfirm'
 }
 
-interface UserInfoFieldValue{
+interface UserInfoFieldValue {
     value: string
     errors: string[]
 }
 
-type UserInformation =  Record<UserInfoFields, UserInfoFieldValue>
+type UserInformation = Record<UserInfoFields, UserInfoFieldValue>
 
 // Validators
 const minLengthValidator = (min: number) => (val: string): string | null =>
@@ -21,11 +21,10 @@ const minLengthValidator = (min: number) => (val: string): string | null =>
 const emailValidator = (val: string): string | null =>
     /@/.test(val) ? null : 'Invalid email address.';
 
-const alphanumericValidator = (val: string): string | null =>
-    /^[a-z0-9]+$/i.test(val) ? null : 'Only alphanumeric is allowed.';
+const alphanumericValidator = (val: string): string | null => /^[a-z0-9]+$/.test(val) ? null : 'Only alphanumeric is allowed.';
 
 // Form Fields
-type ValidateFunc = (str: string, data?: UserInformation) => (string|null)[];
+type ValidateFunc = (str: string, data?: UserInformation) => (string | null)[];
 interface Field {
     key: UserInfoFields,
     label: string,
@@ -67,7 +66,7 @@ const getDefaultValue = () => fields.reduce((acc, { key }) => {
 async function submitForm(
     formData: Record<UserInfoFields, string>
 ) {
-    const {username, email, password, passwordConfirm} = formData;
+    const { username, email, password, passwordConfirm } = formData;
     try {
         const response = await fetch(
             'https://www.greatfrontend.com/api/questions/sign-up',
@@ -98,23 +97,25 @@ const SignUpForm = () => {
     // other approaches: 
     // 1) assign default value as a constant out side from react or 
     // 2) useMemo
-    const [userInfo, setUserInfo] = useState<UserInformation>(()=> getDefaultValue());
+    const [userInfo, setUserInfo] = useState<UserInformation>(() => getDefaultValue());
 
     return (
-        <form>
+        <form className={styles.container}>
             {/* 💡TODO: Consider using a third-party library for form data management and validation to streamline handling.
  */}
             {fields.map(({ key, label, validation }) => {
                 const { value, errors } = userInfo[key];
+                const errorMsg = errors.filter(error => error !== null).join(' ')
+                const errorElId = `${key}-error`
                 return (
-                    <div key={key}>
+                    <div key={key} className={styles.field}>
                         <label htmlFor={key}>{label}</label>
                         <input id={key}
                             value={value}
+                            aria-describedby={errorElId}
                             onChange={
                                 (e) => {
                                     const newVal = e.target.value;
-                                    if (!newVal) return;
 
                                     const errorList = validation(newVal, userInfo);
 
@@ -124,11 +125,11 @@ const SignUpForm = () => {
                                 }
                             }
                         />
-                        <div>
-                            {errors.length > 0 && errors.map(error =>
-                                <p key={error}>{error}</p>
-                            )}
-                        </div>
+                        {errorMsg && (
+                            <div className={styles.error} id={errorElId}>
+                                {errorMsg}
+                            </div>
+                        )}
                     </div>
                 )
             }

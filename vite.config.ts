6603985818d387798df6c4b9e7be7e240fe0edc3
eury.tang <eury0811@gmail.com>
2025-02-18 17:59:51 +0900

    chore(dep): downgrade react for stable sb, adjust ts for vitest
    cfds

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import {defineConfig} from 'vitest/config'
-import {svelte} from '@sveltejs/vite-plugin-svelte'
-import {svelteTesting} from '@testing-library/svelte/vite'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [svelte(), svelteTesting()],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest-setup.js'],
--- vitest-setup.js
+++ vitest-setup.js
@@ -1,1 +1,12 @@
-import '@testing-library/jest-dom/vitest'
+import {afterEach, expect} from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+
+import * as matchers from '@testing-library/jest-dom/matchers'
+
+expect.extend(matchers)
+
+// Automatically clean up after each test
+afterEach(() => {
+  cleanup();
+});
